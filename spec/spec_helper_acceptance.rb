43a7ea853c0d1165b880c9b8231309712f2146d8
Matt Dainty <matt@bodgit-n-scarper.com>
2017-10-17 08:43:02 +0100

    Sync infrastructure

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,12 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
+require 'beaker/puppet_install_helper'
 
 hosts.each do |host|
-  install_puppet
+  # Just assume the OpenBSD box has Puppet installed already
+  if host['platform'] !~ /^openbsd-/i
+    run_puppet_install_helper_on(host)
+  end
 end
 
 RSpec.configure do |c|
@@ -13,13 +17,13 @@ RSpec.configure do |c|
   c.before :suite do
     hosts.each do |host|
       puppet_module_install(:source => proj_root, :module_name => 'sssd')
-      on host, puppet('module','install','puppetlabs-stdlib'),                 { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','bodgit-bodgitlib'),                  { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','richardc-datacat'),                  { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','herculesteam-augeasproviders_core'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','bodgit-dbus'),                       { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','bodgit-openldap'),                   { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','trlinkin-nsswitch'),                 { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'),                 { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'bodgit-bodgitlib'),                  { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'richardc-datacat'),                  { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'herculesteam-augeasproviders_core'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'bodgit-dbus'),                       { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'bodgit-openldap'),                   { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'trlinkin-nsswitch'),                 { :acceptable_exit_codes => [0,1] }
       scp_to(host, File.join(proj_root, 'spec/fixtures/files/example.ldif'), '/root/example.ldif')
     end
   end
