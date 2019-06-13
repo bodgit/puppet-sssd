6e4c9b754d93d0112682d254a52beb345cc2eb29
Jianwen WEI <weijianwen@gmail.com>
2019-06-13 11:53:16 +0800

    Add Debian into OS param.pp .

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,6 +24,28 @@ class sssd::params {
         default   => false,
       }
     }
+    'Debian': {
+      $conf_dir              = '/etc/sssd'
+      $conf_file             = "${conf_dir}/sssd.conf"
+      $dbus_package_name     = 'sssd-dbus'
+      $package_name          = 'sssd'
+      $service_name          = 'sssd'
+      $socket_services       = ['nss', 'pam', 'sudo', 'autofs', 'ssh', 'pac', 'secrets'].reduce({}) |Hash $memo, SSSD::Type $service| {
+        $memo + {
+          $service => $service ? {
+            'pam'   => [
+              "sssd-${service}.socket",
+              "sssd-${service}-priv.socket",
+            ],
+            default => "sssd-${service}.socket",
+          }
+        }
+      }
+      $use_socket_activation = $::service_provider ? {
+        'systemd' => true,
+        default   => false,
+      }
+    }
     default: {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
